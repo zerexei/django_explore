1218779f56f8b83f9225e4e4f8bb3cb787371865
Angelo Arcillas <angeloarcillas64@gmail.com>
2026-01-09 12:51:50 +0800

    Rename project to django_explore

--- django_explore/urls.py
+++ django_explore/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for authproject project.
+URL configuration for django_explore project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
